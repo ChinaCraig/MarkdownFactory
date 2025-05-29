278a3b7b26d5ad1a7f8b1bd1c22b95b7055c8a6b
craig <zhangxinge5566@163.com>
2025-05-29 13:42:47 +0800

    修正启动后自动执行数据库脚本文件

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ class Config:
     MYSQL_HOST = os.environ.get('MYSQL_HOST') or '192.168.16.20'
     MYSQL_PORT = os.environ.get('MYSQL_PORT') or '3306'
     MYSQL_USER = os.environ.get('MYSQL_USER') or 'root'
-    MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD') or '！200808Xx'
+    MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD') or '!200808Xx'
     MYSQL_DATABASE = os.environ.get('MYSQL_DATABASE') or 'markdown_factory'
     
     SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DATABASE}?charset=utf8mb4'
